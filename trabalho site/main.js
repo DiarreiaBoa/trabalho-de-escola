2b23f973384676f3a9ecaeb0a4dd4a7730ffcb08
Victor Leonardo Gonçalves N46 2°B <128704367+DiarreiaBoa@users.noreply.github.com>
2023-11-09 09:20:14 -0300

    Add files via upload

--- trabalho site/main.js
+++ trabalho site/main.js
@@ -1,18 +1,38 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio) {
+const elemento  = document.querySelector(seletorAudio).play();
 }
+
+else{
+    alert('elemento nao encontrado');
+}
+ if(elemento != null & elemento.localname === 'audio' ) {
+    elemento.play ();
+ }  
+
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-//para
+
 for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-    const idAudio = `#som_${instrumento}`; //template string
+    const idAudio = `#som_${instrumento}`;
 
     tecla.onclick = function () {
         tocaSom(idAudio);
     }
+
+    tecla.onkeydown = funcition (){
+
+            tecla.classlist.add('space' || evento.code === 'enter'  )
+    }
+
+        tecla.classlist.add('ativa');
+    }
+
+    tecla.onkeyup = funcition (){
+
+        tecla.classlist.remove('ativa');
 
 }
